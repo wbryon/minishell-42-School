1641b0548150aa1833f89fc4feab55930e91c153
Willodean Bryon <wbryon@si-f1.kzn.21-school.ru>
2021-05-14 19:26:24 +0300

    added cosmetic changes to 'minishell.c', 'minishell.h'

--- minishell.c
+++ minishell.c
@@ -1,19 +1,45 @@
 #include "minishell.h"
 
+int		sort_ft(char **buf)
+{
+	if (ft_strncmp(buf[0], "ls", ft_strlen(buf[0])) == 0)
+		printf("ls-----\n");
+	else
+		printf("diff-----\n");
+	return (0);
+}
+
 int	main(int argc, char **argv, char **env)
 {
-	int		status;
+	int		ret;
+	char	*current_path;
 	char	*line;
-	t_main	main;
-
-	(void)argc;
+	char	tmp[MAXSIZE];
+	int		i;
+	char	**buf;
+	struct	termios term;
+	
+	tcgetattr(0, &term);
+	term.c_lflag &= ~(ECHO);
+	term.c_lflag &= ~(ICANON);
+	tcsetattr(0, TCSANOW, &term);
 	(void)argv;
-	while (status)
+	line = NULL;
+	ret = argc;
+	i = -1;
+
+	/*while (env[++i])
 	{
-		ft_putstr_fd("sh> ", 1);
-		get_next_line(1, line);
-		parcer(line, &main);
-		executor(&main, env);
+			write(1, env[i], ft_strlen(env[i]));
+			write(STDOUT, "\n", 1);
+	}*/
+	while (TRUE)
+	{
+		//ft_putstr_fd(getcwd(current_path, MAXSIZE), STDIN);
+		ft_putstr_fd("sh> ", STDOUT);
+		ret = read(STDIN, tmp, BUFFER_SIZE);
+		write(STDOUT, tmp, ret);
 	}
+	write(STDOUT, "\n", 1);
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -8,6 +8,7 @@
 # include <sys/wait.h>
 # include <signal.h>
 # include <sys/stat.h>
+# include <string.h>
 # include <dirent.h>
 # include <sys/errno.h>
 # include <sys/ioctl.h>
@@ -15,11 +16,26 @@
 # include <curses.h>
 # include <term.h>
 # include "libft/libft.h"
-# include "gnl/get_next_line.h"
 
-typedef struct  s_main
-{
-    
-}               t_main;
+# define MAXSIZE		1024
+# define BUFFER_SIZE    32
+
+# define FALSE			0
+# define TRUE			1
+
+# define ERROR			-1
+# define SUCCESS 		1
+
+# define STDIN 			0
+# define STDOUT 		1
+# define STDERR 		2
+
+# define SYNTAX_ERROR	"Syntax error: near unexpected token!"
+# define QUOTE_ERROR 	"The number of Quotes is odd!"
+# define MALLOC_ERROR 	"Memory malloc fail!"
+# define PIPE_ERROR 	"Pipe function is failed!"
+# define FORK_ERROR 	"Fork function is failed!"
+
+extern  char *environ;
 
 #endif
