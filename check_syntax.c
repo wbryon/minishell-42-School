5d3bf5c5f9831835224499124805247861e42efe
Willodean Bryon <wbryon@si-n2.kzn.21-school.ru>
2021-05-27 13:48:34 +0300

    commit before watching parser2, created by felisabe

--- check_syntax.c
+++ check_syntax.c
@@ -3,9 +3,6 @@
 int	check_quotes(t_all *all)
 {
 	int	i;
-<<<<<<< HEAD
-=======
->>>>>>> f1e51a8201dd8a443986cb8fbeb7c6ff28fcb530
 
 	i = -1;
 	while (all->history->current[++i] != '\0')
@@ -13,17 +10,9 @@ int	check_quotes(t_all *all)
 		if (all->history->current[i] == '\'')
 		{
 			if (all->history->current[i - 1] && all->history->current[i - 1] == '\\')
-<<<<<<< HEAD
-			{
-				if (all->s_c.s_quotes % 2 == 0)
-					continue;
-			}
-			s_quotes++;
-=======
 				i++;
 			else
 				all->s_c.s_quotes++;
->>>>>>> f1e51a8201dd8a443986cb8fbeb7c6ff28fcb530
 		}
 	}
 	i = -1;
@@ -32,20 +21,53 @@ int	check_quotes(t_all *all)
 		if (all->history->current[i] == '"')
 		{
 			if (all->history->current[i - 1] && all->history->current[i - 1] == '\\')
-<<<<<<< HEAD
-			{
-				if (all->s_c.d_quotes % 2 == 0)
-					continue;
-			}
-			d_quotes++;
-=======
 				i++;
 			else
 				all->s_c.d_quotes++;
->>>>>>> f1e51a8201dd8a443986cb8fbeb7c6ff28fcb530
 		}
 	}
 	if (all->s_c.s_quotes % 2 != 0 || all->s_c.d_quotes % 2 != 0)
 		return (1);
 	return (0);
 }
+
+// int	check_quotes(t_all *all)
+// {
+// 	int	i;
+
+// 	i = -1;
+// 	while (all->history->current[++i] != '\0')
+// 	{
+// 		if (all->history->current[i] == '\'')
+// 		{
+// 			if (all->history->current[i - 1] && all->history->current[i - 1] == '\\')
+// 			{
+// 				if (all->s_c.s_quotes % 2 == 0)
+// 					continue;
+// 			}
+// 			all->s_c.s_quotes++;
+// 				i++;
+// 			else
+// 				all->s_c.s_quotes++;
+// 		}
+// 	}
+// 	i = -1;
+// 	while (all->history->current[++i] != '\0')
+// 	{
+// 		if (all->history->current[i] == '"')
+// 		{
+// 			if (all->history->current[i - 1] && all->history->current[i - 1] == '\\')
+// 			{
+// 				if (all->s_c.d_quotes % 2 == 0)
+// 					continue;
+// 			}
+// 			all->s_c.d_quotes++;
+// 				i++;
+// 			else
+// 				all->s_c.d_quotes++;
+// 		}
+// 	}
+// 	if (all->s_c.s_quotes % 2 != 0 || all->s_c.d_quotes % 2 != 0)
+// 		return (1);
+// 	return (0);
+// }
